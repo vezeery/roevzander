b05341ba0c2b051f99587d671074c8039d64cb32
vezeery <subaruwrxsti318@gmail.com>
2026-05-20 13:56:28 +0700

    rollback version to migrate into vercell hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',   
-  basePath: '/roevzander',
-  trailingSlash: true,   // Required for GitHub Pages
-  images: {
-    unoptimized: true,   // Required because GitHub has no image server
-  },
-  // If your URL is username.github.io/roevzander, add this:
-  // basePath: '/roevzander', 
-};
+const nextConfig = {};
 
 export default nextConfig;
-
-
